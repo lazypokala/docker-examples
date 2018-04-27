4579846db03b118546c087caf92726c9670a2980
Satish <satish.pokala@coopdigital.co.uk>
2018-04-27 15:12:32 +0100

    New version

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app.debug = True
 
 @app.route('/')
 def main():
-    return 'Hi ! I\'m a Flask application in Docker!!'
+    return 'Hi ! I\'m a Flask application in a Docker container with Auto Build!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
